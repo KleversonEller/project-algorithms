e151f7c3f0cc0254ecb4b3fd19bc621455bb7125
Kleverson Eller <kleverson.a16@gmail.com>
2022-11-28 00:28:54 -0300

    test: :white_check_mark: Criando teste (test_encrypt_message)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
+    assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
+    with pytest.raises(TypeError):
+        encrypt_message("AABBCC", "")
+    with pytest.raises(TypeError):
+        encrypt_message(None, 3)
+    with pytest.raises(TypeError):
+        encrypt_message(1, 1)
