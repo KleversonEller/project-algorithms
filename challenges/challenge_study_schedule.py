8a240bd7ee86874af1a7101f5399afb4ffcdce85
Kleverson Eller <kleverson.a16@gmail.com>
2022-11-28 00:28:05 -0300

    feat: :sparkles: Criando função (study_schedule)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    student_online = 0
+    for student_time in permanence_period:
+        if not isinstance(student_time[0], int) or not isinstance(
+            student_time[1], int
+        ):
+            return None
+        if student_time[0] <= target_time and student_time[1] >= target_time:
+            student_online += 1
+
+    return student_online
